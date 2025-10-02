8bb99797a753c175778ef96243fd9a660b193643
Enrique🐬 <enripl05@gmail.com>
2025-10-02 15:35:13 +0200

    Delete JavaScript_semana3 directory

--- JavaScript_semana3/Test/Data/init_indexhtml.js
+++
@@ -1,3 +0,0 @@
-switch_display_mode('titulomenu','block','off');
-switch_display_mode('div-menu','block','off');
-switch_display_mode('form','block','off');
--- JavaScript_semana3/Test/Unit/init_indexhtml.js
+++
@@ -1,2 +0,0 @@
-switch_display_mode('titulomenu','block','off');
-switch_display_mode('div-menu','block','off');
